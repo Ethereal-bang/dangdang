b29530f4ffe4f689fca109d2eaaca5e68cb288e2
Ethereal-bang <939847757@qq.com>
2022-04-07 10:57:27 +0800

    update: 巨幕广告、头部导航栏

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,72 @@
+import styles from "./Header.module.css";
+
+export const Header = () => {
+    return (
+        <>
+            {/*广告*/}
+            <header>
+                <img alt={"ad"} src={"http://img61.ddimg.cn/2022/4/2/2022040217391412915.jpg"}/>
+            </header>
+            {/*导航栏*/}
+            <section>
+                <div>
+                    <span>送至：</span>
+                </div>
+                <div>
+                    <span>
+                        欢迎光临当当，请
+                        <a href="./">登录</a>
+                    </span>
+                    <span>成为会员</span>
+                    <ul>
+                        <li>
+                            <a href={"./"} className={styles.cart}>
+                                购物车
+                            </a>
+                        </li>
+                        <li>
+                            <a href={"./"}>
+                                我的订单
+                            </a>
+                        </li>
+                        <li>
+                            <span>
+                                我的云书房
+                            </span>
+                        </li>
+                        <li>
+                            <span>
+                                我的当当
+                            </span>
+                        </li>
+                        <li>
+                            <span>
+                                当当拼团
+                            </span>
+                        </li>
+                        <li>
+                            <span>
+                                企业采购
+                            </span>
+                        </li>
+                        <li>
+                            <span>
+                                小说投稿
+                            </span>
+                        </li>
+                        <li>
+                            <span>
+                                客户服务
+                            </span>
+                        </li>
+                        <li>
+                            <span>
+                                切换无障碍
+                            </span>
+                        </li>
+                    </ul>
+                </div>
+            </section>
+        </>
+    )
+}
