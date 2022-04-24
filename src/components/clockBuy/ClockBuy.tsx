83c7f46f22cdeaea525f2801e4f09e6628906fd9
Ethereal-bang <939847757@qq.com>
2022-04-24 11:33:59 +0800

    update: "抢购"

--- src/components/clockBuy/ClockBuy.tsx
+++ src/components/clockBuy/ClockBuy.tsx
@@ -1,8 +1,40 @@
 import styles from "./ClockBuy.module.css";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 const timeOrder = ["00:00场", "08:00场", "12:00场", "16:00场", "20:00场",];
 
+interface Book {
+    img: [string],
+    name: string,
+    price_now: number,
+    price_old: number,
+    process: string,    // 已抢购d%
+}
+
+interface Ad {
+    name: string,
+    img: string,
+}
+
 export const ClockBuy = () => {
+    const [books, setBooks] = useState<[Book]>();
+    const [ads1, setAds1] = useState<[Ad]>();
+    const [ads2, setAds2] = useState<[Ad]>();
+
+    useEffect(() => {
+        axios.get("http://localhost:3001/goods/getByType/clock")
+            .then(res => {
+                setBooks(res.data.data.list);
+            })
+
+        axios.get("http://localhost:3001/ad/getByPos/clock")
+            .then(res => {
+                setAds1(res.data.data.list.slice(4));
+                setAds2(res.data.data.list.slice(4, 8));
+            })
+    }, [])
+
     return (
         <>
             <header className={styles["header"]}>
@@ -25,6 +57,36 @@ export const ClockBuy = () => {
                 {/*厂商周*/}
                 <span className={styles["week"]}>厂商周</span>
             </header>
+            {/*图书*/}
+            <section className={styles["container"]}>
+                {books?.map((bookInfo, index) => (
+                    <div key={index} className={styles["bookBox"]}>
+                        <a className={styles["img"]} href={"./"} title={bookInfo.name}>
+                            <img src={bookInfo.img[0]} alt={"book info"} />
+                        </a>
+                        <div className={styles["line"]}>
+                            <span/>
+                            <div className={styles["process"]}>{bookInfo.process}</div>
+                        </div>
+                        <a className={styles["name"]} href={"./"} title={bookInfo.name}>{bookInfo.name}</a>
+                        <div className={styles["price"]}>
+                            ￥
+                            <span>{bookInfo.price_now}</span>
+                            <span>{bookInfo.price_old}</span>
+                        </div>
+                    </div>
+                ))}
+            </section>
+            {/*广告*/}
+            <ul className={styles["ad"]}>
+                {ads1?.map((ad, index) => (
+                    <li key={index}>
+                        <a href={"./"} title={ad.name}>
+                            <img src={ad.img} alt={ad.name}/>
+                        </a>
+                    </li>
+                ))}
+            </ul>
         </>
     )
 }
