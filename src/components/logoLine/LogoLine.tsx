d887eb9c6928f8ad6ddcdeda77e4f8ecb2c7e9b3
Ethereal-bang <939847757@qq.com>
2022-05-07 14:41:06 +0800

    update: "搜索"

--- src/components/logoLine/LogoLine.tsx
+++ src/components/logoLine/LogoLine.tsx
@@ -1,7 +1,29 @@
 import styles from "./LogoLine.module.css";
 import {Link} from "react-router-dom";
+import {useEffect, useState} from "react";
+import axios from "axios";
+
+interface SearchResItem {
+    name: string,
+    _id: string,
+}
 
 export const LogoLine = () => {
+    const [searchVal, setSearchVal] = useState<string>("");
+    const [searchRes, setSearchRes] = useState<SearchResItem[]>([]);
+
+    // input一改变自动开始模糊匹配
+    useEffect(() => {
+        axios.get("http://localhost:3001/goods/search", {
+            params: {
+                name: searchVal,
+            }
+        })
+            .then(res => {
+                setSearchRes(res.data.data);
+            })
+    }, [searchVal]);
+
     return (
         <div className={styles["logoLine"]}>
             {/*logo*/}
@@ -11,7 +33,20 @@ export const LogoLine = () => {
                 {/*Input*/}
                 <section className={styles["search_box"]}>
                     <div>
-                        <input placeholder={"白茶新书"}/>
+                        <input
+                            placeholder={"白茶新书"}
+                            value={searchVal}
+                            onChange={(e) => setSearchVal(e.target.value)} />
+                        {/*搜索结果*/}
+                        <ul className={styles["search_ul"]}>
+                            {searchRes?.map(item => (
+                                <li key={item._id}>
+                                    <Link to={`/goods/${item._id}`}>
+                                        {item.name}
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
                         <span>
                             全部分类
                             <i />
